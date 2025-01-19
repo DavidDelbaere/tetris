cf7c0ee5c057e36e1cf2cafffc5f2209a35e56c6
Nick Pighin <npighin@gmail.com>
2025-01-18 23:29:09 -0500

    mostly done

--- tetrisMaybeFinal.py
+++ tetrisMaybeFinal.py
@@ -55,6 +55,24 @@ def find_perfect_moves(board, piece):
             # firstLeftTile not found, increment firstLeftTile
             firstLeftTile += 1
 
+    #determines the difference in height between two columns
+    for w in range(len(piece[0])):
+        columnDif = 0
+        leftColumnTop = 0
+        nowColumnTop = 0
+                
+        columnHeight = len(piece)-1
+        while(piece[columnHeight][0] == 0):
+            columnHeight -= 1
+        leftColumnTop = columnHeight
+                
+        columnHeight = len(piece)-1
+        while(piece[columnHeight][w] == 0):
+            columnHeight -= 1
+        nowColumnTop = columnHeight
+
+        columnDif = nowColumnTop - leftColumnTop
+
     #initialize variables
     validPosList = []
 
@@ -75,16 +93,16 @@ def find_perfect_moves(board, piece):
             #determines whether the right side of a block would lie outside
             #the play area in a potential position
             if(x + w >= len(board[0])):
-                print("case1")
+                #print(str(x) + ": case1")
                 valid = False
                 break
             else:
                 #determines whether the bottom of a block would lie beneath
                 #the play area in a potential position
                 if((y + firstLeftTile)>len(board)):
-                        print("case2")
-                        valid = False
-                        break
+                    #print(str(x) + ": case2")
+                    valid = False
+                    break
 
                 else:
                     #iterates through the rows in a block
@@ -92,38 +110,53 @@ def find_perfect_moves(board, piece):
 
                         #determines whether a tile on the board(that is filled) would be intersected
                         #by a potential block placement
-                        if((board[y-h-1][x+w] == 1) and (piece[len(piece)-h-1][w] == 1)):
-                            print("case3")
+                        if((board[y-len(piece)+h][x+w] == 1) and (piece[h][w] == 1)):
+                            #print(str(x) + ":" + str(h)+ ": case3")
                             valid = False
                             break
-                            
+
                         #determines whether the selected tile on a block would leave a hole beneath it
                         #(relative to the board) in a potential block placement
-                        elif(y != len(board)):
-                            if(((board[y-len(piece)+h+1][x+w] == 0) and (piece[len(piece)-h-1][w] == 1))):
+                        elif(y+columnDif < len(board)):
+                            if(((board[y-len(piece)+h+1+columnDif][x+w] == 0) and (piece[h][w] == 1))):
                                 
                                 #if this is the bottom row of the block
                                 if(h == (len(piece) - 1)):
-                                    print("case4")
+                                    #print(str(x) + ": case4")
                                     valid = False
                                     break
 
                                 #determines whether there is a tile within the block beneath the currently selected block tile
                                 elif((piece[h][w] == 1) and (piece[h+1][w] == 0)):
-                                    print("case5")
+                                    #print(str(x) + ": case5")
                                     valid = False
                                     break
                         
                         elif(h != len(piece)-1):
                             if((piece[h][w] == 1) and (piece[h+1][w] == 0)):
-                                print("case6")
+                                #print(str(x) + ": case6")
                                 valid = False
                                 break
 
+        # check if the spots above the potential piece are occupied and therefore the spot is impossible to reach
+        # for each column of the piece: find the highest point that is occupied
+        # from this point, go all the way up in the board until you each top of board OR spot is occupied on board
+        for piece_col in range(len(piece[0])):
+            highest_point = len(piece)
+            for piece_row in range(len(piece)):
+                if (piece[piece_row][piece_col] == 1):
+                    break
+                else:
+                    highest_point -= 1
+            for board_row in range(len(board) - highest_point - 1, -1, -1):
+                if (board[board_row][x+piece_col] == 1):
+                    valid = False
+                    break
+            if (valid == False):
+                break
+
         if(valid):
             validPosList.append(x)
-        
-    print(validPosList)
 
     if(len(validPosList) != 0):
         bestPos = 0
@@ -457,12 +490,16 @@ class BlocksGroup(pygame.sprite.OrderedUpdates):
         pot_best_moves.append((2, perfectMove3))
         pot_best_moves.append((3, perfectMove4))
 
-        bestMove = (pot_best_moves[0][0], pot_best_moves[0][1][0], pot_best_moves[0][1][1])
+        bestMove = (0, 0, -1)
         for x in range(len(pot_best_moves)):
-            if(pot_best_moves[x][1][0] != "none"):
-                if(pot_best_moves[x][1][1] < bestMove[2]):
+            if(type(pot_best_moves[x][1][1]) is not str):
+                if((pot_best_moves[x][1][1] < bestMove[2]) or (bestMove[2] < 0)):
                     bestMove = (pot_best_moves[x][0], pot_best_moves[x][1][0], pot_best_moves[x][1][1])
         #print(pot_best_moves)
+        if(bestMove[2] < 0):
+            bestMove = "no best move"
+
+        print(bestMove)
 
         self.update_grid()
         self._check_line_completion()
